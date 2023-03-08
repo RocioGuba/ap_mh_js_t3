e83dd7bdffe840ad7db1857bf0d6c65540ce230c
RocioGuba <rocioguba2511@gmail.com>
2023-03-08 16:53:01 -0300

    details-arreglo

--- script/details.js
+++ script/details.js
@@ -9,11 +9,11 @@ let eventos = (data.events).find(events => events._id == id);
 const details = document.querySelector("contenedor__tarjetas");
 details.innerHTML = `
 <div class="card" style="width: 18rem;">
-        <img src="${details[0].image}" class="card-img-top" alt="...">
+        <img src="${details.image}" class="card-img-top" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${details[0].name}</h5>
-          <p class="card-text">${details.[0].description}</p>
-          <p class="card-text">${details.[0].category}</p>
+          <h5 class="card-title">${details.name}</h5>
+          <p class="card-text">${details.description}</p>
+          <p class="card-text">${details.category}</p>
           
         </div>
       </div>`
