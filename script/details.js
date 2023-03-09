0accf4adeab69708b054db7f296502205f2a7c81
RocioGuba <rocioguba2511@gmail.com>
2023-03-09 14:23:41 -0300

    cambios

--- script/details.js
+++ script/details.js
@@ -1,19 +1,41 @@
-let queryString = location.search
+function getData(){
+    let idCard = 1
+    data.eventos.map(evento => evento.id = idCard++)
 
-let params = new URLSearchParams(queryString)
+    let id = location.search.split("?id=").filter(Number)
 
-let id = params.get("_id");
+    let selectID = Number(id[0])
+    console.log(selectID)
 
-let eventos = (data.events).find(events => events._id == id);
+    let evento = data.eventos.find((evento) => {
+        return evento.id == selectID
+    })
 
-const details = document.querySelector("contenedor__tarjetas");
-details.innerHTML = `
-<div class="card" style="width: 18rem;">
-        <img src="${details.image}" class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">${details.name}</h5>
-          <p class="card-text">${details.description}</p>
-          <p class="card-text">${details.category}</p>
-          
-        </div>
-      </div>`
+
+
+let templateHTML = `
+<div class="card mb-3 card shadow p-3 mb-5 bg-white rounded my-4" style="max-width: 600px">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="${evento.image}" class="img-fluid rounded-start mt-5 p-4" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">${evento.name}</h5>
+        <ul>
+        <li>Date:${evento.date}</li>
+        <li>Description:${evento.description}</li>
+        <li>Category:${evento.category}</li>
+        <li>Capacity:${evento.capacity}</li>
+        <li>Assistence or estimate ${evento.assistance}:</li>
+        <li>Price:${evento.price}</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+`
+
+document.getElementById('contenedorTarjetasD').innerHTML= templateHTML
+}
+getData ()
